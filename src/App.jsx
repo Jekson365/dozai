be755d4a09cfb66c25e8c66d5484ef5a115e308e
dadsasdl <jemali.ergemlidze.1@btu.edu.ge>
2022-12-10 14:00:35 +0400

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import { SectionHome } from './sections/homecomponents/SectionHome'
 import './styles/main.scss'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
-
+  useEffect(()=> {
+    AOS.init()
+  })
   return (
     <>
       <SectionHome/>
--- src/sections/homecomponents/HomeContent.jsx
+++ src/sections/homecomponents/HomeContent.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 
 export const HomeContent = () => {
-  return (
-    <>
-    <div className="homecontent d-flex h-100 align-items-center justify-content-center">
-        <img src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633de349ae424383a2e10038_Smiling-Girl-p-500.png'/>
-        <h1 
-        className='pt-5'
-        style={{"fontSize":"30vw","color":"",'zIndex':"0"}}>Dozai</h1>
-    </div>
-    <div className="container images d-flex justify-content-between  h-100 align-items-center">
-        <img 
-        src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633e0700e8fb630be246f1f9_Untitled-3.png'/>
-        <img src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633e0700f4a2f3abc72a0819_Untitled-1.png'/>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="homecontent d-flex h-100 align-items-center justify-content-center">
+                <img 
+                data-aos="fade-up"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="2000"
+                src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633de349ae424383a2e10038_Smiling-Girl-p-500.png' />
+                <h1
+                    className='pt-5'
+                    style={{ "fontSize": "30vw", "color": "", 'zIndex': "0" }}>Dozai</h1>
+            </div>
+            <div className="container images d-flex justify-content-between  h-100 align-items-center">
+                <img
+                    src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633e0700e8fb630be246f1f9_Untitled-3.png' />
+                <img src='https://assets.website-files.com/62e7dd8eb96f150931bf8901/633e0700f4a2f3abc72a0819_Untitled-1.png' />
+            </div>
+        </>
+    )
 }
