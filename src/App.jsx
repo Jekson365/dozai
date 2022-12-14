d2cc012329da61c4af3f42ccd1f4cf0054902a13
dadsasdl <jemali.ergemlidze.1@btu.edu.ge>
2022-12-14 19:00:50 +0400

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
   return (
     <>
     
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <Mycontext.Provider value={{ cart, setCart, setNav, nav }}>
           <div className={`navbar ${nav ? "on" : "off"}`}>
-            <Link to={'/home'} data-aos='fade-down' data-aos-duration='1000'>Home</Link>
-            <Link to={'/about'} data-aos='fade-down' data-aos-duration='1200'>About</Link>
-            <Link to={'/work'} data-aos='fade-down' data-aos-duration='1400'>Work</Link>
+            <Link to={'/dozai/home'} data-aos='fade-down' data-aos-duration='1000'>Home</Link>
+            <Link to={'/dozai/about'} data-aos='fade-down' data-aos-duration='1200'>About</Link>
+            <Link to={'/dozai/work'} data-aos='fade-down' data-aos-duration='1400'>Work</Link>
           </div>
           <Routes>
             <Route path='/home' element={<Home />} />
@@ -35,7 +35,7 @@ function App() {
             <Route path='/work' element={<Projects />} />
           </Routes>
         </Mycontext.Provider>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
